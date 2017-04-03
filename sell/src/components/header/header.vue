9d3916ec35c02e8433920e1f49257bc9a130b929
laclys <laclys@126.com>
2017-04-03 13:19:24 +0800

    added
    header component complated

--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -18,106 +18,178 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
+      <div v-if="seller.supports" class="support-count">
+        <span class="count">{{seller.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
     </div>
     <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+    <img :src="seller.avatar" width="100%" height="100%">
     </div>
   </div>
 </template>
 
 
 <script>
-    export default {
-    
-        props: {
-    
-            seller: {
-    
-                type: Object
-    
-            }
-    
-        },
+  export default {
 
-        created() {
-            this.classMap=['decrease','discount','special','invoice','guarantee'];
-        }
-    
-    };
+    props: {
+
+      seller: {
+
+        type: Object
+
+      }
+
+    },
+
+    created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+    }
+
+  };
+
 </script>
 <style lang="scss" rel="stylesheet/scss">
-        @import "../../common/sass/mixin.scss";
+  @import "../../common/sass/mixin.scss";
 
-       .header{
-           color:#fff;
-           background:#000;
-           .content-wrapper{
-               padding:24px 12px 18px 24px;
-               font-size:0px;
-               .avatar{
-                   display:inline-block;
-                   vertical-align:top;
-                   img{
-                       border-radius:2px;
-                   }
-               }
-               .content{
-                   display:inline-block;
-                   margin-left:16px;
-                   .title{
-                       margin:2px 0 8px 0;
-                       .brand{
-                           display:inline-block;
-                           vertical-align:top;
-                           width:30px;
-                           height:18px;
-                           @include bg-image('brand');
-                           background-size: 30px 18px;
-                           background-repeat:no-repeat;
-                       }
-                           .name{
-                               margin-left:6px;
-                               font-size:16px;
-                               line-height:18px;
-                               font-weight:bold;
-                           }
-                   }
-                   .description{
-                       margin-bottom:10px;
-                       line-height:12px;
-                       font-size:12px;
-                   }
-                   .support{
-                       .icon{
-                           display:inline-block;
-                           vertical-align:top;
-                           width:12px;
-                           height:12px;
-                           margin-right:4px;
-                           background-size:12px 12px;
-                           background-repeat:no-repeat;
-                           &.decrease{
-                               @include bg-image('decrease_1');
-                           }
-                           &.discount{
-                               @include bg-image('discount_1');
-                           }
-                           &.guarantee{
-                               @include bg-image('guarantee_1');
-                           }
-                           &.invoice{
-                               @include bg-image('invoice_1');
-                           }
-                           &.special{
-                               @include bg-image('special_1');
-                           }
-                       }
-                       .text{
-                           line-height:12px;
-                           font-size:12px;
-                           
-                       }
-                   }
-               }
-           }
-       }
+  .header {
+      position:relative;
+    color: #fff;
+    background:rgba(7,17,27,0.5);
+    .content-wrapper {
+      padding: 24px 12px 18px 24px;
+      font-size: 0px;
+      position: relative;
+      .avatar {
+        display: inline-block;
+        vertical-align: top;
+        img {
+          border-radius: 2px;
+        }
+      }
+      .content {
+        display: inline-block;
+        margin-left: 16px;
+        .title {
+          margin: 2px 0 8px 0;
+          .brand {
+            display: inline-block;
+            vertical-align: top;
+            width: 30px;
+            height: 18px;
+            @include bg-image('brand');
+            background-size: 30px 18px;
+            background-repeat: no-repeat;
+          }
+          .name {
+            margin-left: 6px;
+            font-size: 16px;
+            line-height: 18px;
+            font-weight: bold;
+          }
+        }
+        .description {
+          margin-bottom: 10px;
+          line-height: 12px;
+          font-size: 12px;
+        }
+        .support {
+          .icon {
+            display: inline-block;
+            vertical-align: top;
+            width: 12px;
+            height: 12px;
+            margin-right: 4px;
+            background-size: 12px 12px;
+            background-repeat: no-repeat;
+            &.decrease {
+              @include bg-image('decrease_1');
+            }
+            &.discount {
+              @include bg-image('discount_1');
+            }
+            &.guarantee {
+              @include bg-image('guarantee_1');
+            }
+            &.invoice {
+              @include bg-image('invoice_1');
+            }
+            &.special {
+              @include bg-image('special_1');
+            }
+          }
+          .text {
+            line-height: 12px;
+            font-size: 10px;
+          }
+        }
+      }
+      .support-count {
+        position: absolute;
+        right: 12px;
+        bottom: 14px;
+        padding: 0 8px;
+        height: 24px;
+        line-height: 24px;
+        border-radius: 14px;
+        background: rgba(0, 0, 0, 0.2);
+        text-align: center;
+        .count {
+          font-size: 10px;
+          vertical-align: top;
+        }
+        .icon-keyboard_arrow_right {
+          font-size: 10px;
+          line-height: 24px;
+          margin-left: 2px;
+        }
+      }
+    }
+    .bulletin-wrapper{
+        position:relative;
+        height:28px;
+        line-height:28px;
+        padding:0 22px 0 12px;
+        white-space:nowrap;
+        overflow:hidden;
+        text-overflow:ellipsis;  
+        background:rgba(1,17,27,0.2);  
+        .bulletin-title{
+            display:inline-block;
+            vertical-align:top;
+            margin-top:8px;
+            width:22px;
+            height:12px;
+            @include bg-image('bulletin');
+            background-size:22px 12px;
+            background-repeat: no-repeat;
+        }
+        .bulletin-text{
+            vertical-align:top;
+            margin:0 4px;
+            font-size:10px;
+        }
+        .icon-keyboard_arrow_right{
+            position:absolute;
+            font-size:10px;
+            right:12px;
+            top:8px;
+        }
+    }
+    .background{
+        position:absolute;
+        top:0;
+        left:0;
+        width:100%;
+        height:100%;
+        z-index:-1;
+        filter:blur(10px);
+    }
+  }
+
 </style>
